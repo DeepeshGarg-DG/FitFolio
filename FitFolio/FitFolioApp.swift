24e9ab85ca4a0756e02a55f90d74edb9526c4633
DeepeshGarg-DG <deepeshgarg49@gmail.com>
2023-11-15 16:43:40 +0530

    Initial Commit

---
+++ FitFolio/FitFolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitFolioApp.swift
+//  FitFolio
+//
+//  Created by Deepesh Garg on 15/11/23.
+//
+
+import SwiftUI
+
+@main
+struct FitFolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
